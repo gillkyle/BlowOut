fed1661788563fc5f124acceda797199a58811d1
mckayvp <mckaypalmer@gmail.com>
2017-11-06 23:06:22 -0700

    Good push

--- BlowOut/Models/Instrument.cs
+++ BlowOut/Models/Instrument.cs
@@ -11,6 +11,7 @@ namespace BlowOut.Models
     public class Instrument
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int instrumentID { get; set; }
         public string desc { get; set; }
         public string type { get; set; }
@@ -24,7 +25,7 @@ namespace BlowOut.Models
     /*
     CREATE TABLE [dbo].[Instrument] (
     [instrumentID] INT          NOT NULL PRIMARY KEY,
-    [dec]          VARCHAR (50) NOT NULL,
+    [desc]          VARCHAR (50) NOT NULL,
     [type]         VARCHAR (4)  NOT NULL,
     [price]        INT        NOT NULL,
     [clientID]     INT          NOT NULL,
